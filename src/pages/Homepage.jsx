fa25635b47cb28080d57f2448ca7a4ad315aa4d0
karmartir <karmartir@gmail.com>
2026-02-21 18:47:16 -0500

    Using CSS Modules

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
+import AppNav from "../components/AppNav";
 import PageNav from "../components/PageNav";
 
 function Homepage() {
   return (
     <div>
       <PageNav />
+      <AppNav />
       WorldWise
       <h1>WorldWise</h1>
+      <Link to="/app">Go to the app</Link>
     </div>
   );
 }
