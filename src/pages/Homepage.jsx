4b6b48db2246a083c03d9ae5dc71d868c0ba6f09
karmartir <karmartir@gmail.com>
2026-02-21 16:56:51 -0500

    main pages, routes, linking between and NavLink

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
+function Homepage() {
+  return (
+    <div>
+      <PageNav />
+      WorldWise
+      <h1>WorldWise</h1>
+    </div>
+  );
+}
+
+export default Homepage;
